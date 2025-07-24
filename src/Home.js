9f0d11e1407b0d50166cc3e2cba678550656671e
Naman Srivastava <namansrivastava1608@gmail.com>
2025-07-24 09:25:37 +0530

    link comp

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <React.Fragment>
             <h1>Welcome to the Home Page!</h1>
             <p>This is a simple React component.</p>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/mainpage">Main Page</Link></li>
+                    <li><Link to="/condition">Condition</Link></li>
+                    <li><Link to="/clock">Clock</Link></li>
+                    <li><Link to="/events">Events</Link></li>
+                    <li><Link to="/form">Form</Link></li>
+                    <li><Link to="/incdec">Increment/Decrement</Link></li>
+                </ul>
+            </nav>
         </React.Fragment>
     )
 } 
