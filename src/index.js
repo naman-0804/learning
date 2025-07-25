3fd8a5506349e3dbdbbbba6c9eeeb46faeacb92a
Naman Srivastava <namansrivastava1608@gmail.com>
2025-07-25 10:26:37 +0530

    axios basics

--- src/index.js
+++ src/index.js
@@ -14,10 +14,11 @@ import Ref from './Ref';
 import Memo from './Memo';
 import Callback from './Callback';
 import "./index.css";
+import Axios from './Axios';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Axios />
     </React.StrictMode>
 );
  
