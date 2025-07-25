2c76913bffbb257d27065cf592ed5eb80834bfe4
Naman Srivastava <namansrivastava1608@gmail.com>
2025-07-25 11:50:05 +0530

    update

--- src/Axiospost.js
+++ src/Axiospost.js
@@ -21,12 +21,22 @@ const Axiospost = () => {
             console.error("Error posting data:", err);
         });
     }
+    const handleUpdate = () => {
+        axios.put("https://jsonplaceholder.typicode.com/todos/1", input)
+        .then((res) => {
+            console.log("Data updated successfully:", res.data);
+        })
+        .catch((err) => {
+            console.error("Error updating data:", err);
+        });
+    }
   return (
     <div>
     <form onSubmit={handlevent}>
         <input type="text" name="fname" placeholder="Enter title" onChange={handle}/>
         <input type="text" name="lname" placeholder="Enter srname" onChange={handle}/>
         <button type="submit">Submit</button>
+        <button onClick={handleUpdate}>update</button>
     </form>
         <p>First Name: {input.fname}</p>
         <p>Last Name: {input.lname}</p>
