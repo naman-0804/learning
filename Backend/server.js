79d147aae593c8a5ea2e846a4920057880457250
Naman Srivastava <namansrivastava1608@gmail.com>
2025-09-12 14:13:11 +0530

    cahnged location

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const fs=require("fs");
 const blog=require('./routes/blog.js')
+const port=3000;
 app.use((req,res,next)=>{
     fs.appendFileSync("Server.log",`${new Date().toString()} ${req.url} ${req.method}\n`)
     next();
@@ -9,7 +10,7 @@ app.use((req,res,next)=>{
 app.use(express.static('public'));
 app.use('/blog',blog); 
 //http://localhost:3000/naman.txt accessible
-const port=3000;
+
 app.get('/',(req,res)=>{
     res.send("Hello world")
 })
