7f9c979f00b6170019c05e80661e79524dd62731
Naman Srivastava <namansrivastava1608@gmail.com>
2025-09-12 09:47:46 +0530

    static file serving

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
+app.use(express.static('public'));
+//http://localhost:3000/naman.txt accessible
 const port=3000;
 app.get('/',(req,res)=>{
     res.send("Hello world")
