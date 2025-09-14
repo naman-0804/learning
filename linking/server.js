1e4b8663c63f3d82f23dd77c23c17c8e6adb203e
Naman Srivastava <namansrivastava1608@gmail.com>
2025-09-14 08:50:40 +0530

    connected mongodb

--- linking/server.js
+++ linking/server.js
@@ -1,15 +1,34 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose';
 const app=express();
 app.use(cors())
 app.use(express.json());
 const port=3000;
+await mongoose.connect("mongodb+srv://namansrivastava1608_db_user:nlq1IdzwCTj4p0ly@cluster0.6cthoda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log("connected to mongodb");
+const userSchema=new mongoose.Schema({
+    id: Number,
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+})
+const User=mongoose.model("User",userSchema);
 app.get('/',(req,res)=>{
     res.send("my server");
 })
-app.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send("Data received");
+app.post('/',async (req,res)=>{
+    const {username,password}=req.body;
+    if (!username || !password) {
+      return res.status(400).send("Username and password required");
+    }
+    const newUser=new User({username,password,id:Date.now()});
+    await newUser.save();
+    console.log("Saved to DB:", newUser);
+    //console.log(req.body);
+    res.send("Data received and saved");
 })
 app.listen(port,()=>{
     console.log(`Listen on port:${port}`)
