e02180eeeed5a8d42063fe8e9e745b99439b9a97
Naman Srivastava <namansrivastava1608@gmail.com>
2025-12-24 11:15:53 +0530

    nav router

--- React/src/App.js
+++ React/src/App.js
@@ -1,17 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Counter from "./usestate";
 import ApiFetcher from "./useffect";
 import RefCounter from "./useref";
 import { useState } from "react";
-import ChildC from "./Proivder_usecontext";  
+import ChildC from "./Proivder_usecontext";
 function App() {
   const [cnt, setCnt] = useState(1);
 
   return (
     <div>
-      <Counter cnt={cnt} setCnt={setCnt} />
-      <ApiFetcher cnt={cnt} />
-      <RefCounter />
-      <ChildC/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/usestate" element={<Counter cnt={cnt} setCnt={setCnt} />} />
+          <Route path="/useeffect" element={<ApiFetcher cnt={cnt} />} />
+          <Route path="/useref" element={<RefCounter />} />
+          <Route path="/usecontext" element={<ChildC />} />
+        </Routes>
+      </BrowserRouter>
+      <nav>
+        <a href="/usestate">useState</a> |{" "}
+        <a href="/useeffect">useEffect</a> |{" "}
+        <a href="/useref">useRef</a> |{" "}
+        <a href="/usecontext">useContext</a>
+      </nav>
     </div>
   );
 }
