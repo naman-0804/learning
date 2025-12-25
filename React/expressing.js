a9f3db957b2206d83751d6715924760ecf70c74d
Naman Srivastava <namansrivastava1608@gmail.com>
2025-12-25 11:16:39 +0530

    basic get

--- React/expressing.js
+++ React/expressing.js
@@ -5,11 +5,7 @@ app.get("/", async (req, res) => {
     try {
         const response = await fetch("https://dummyjson.com/c/3029-d29f-4014-9fb4")
         const data =await response.json()
-        res.json({
-            query:req.query,
-            apiData: data
-        })
-        console.log("User ID received");
+        res.json(data)
        
     } catch (error) {
         res.status(500).send("ISR")
