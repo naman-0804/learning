77c43738204c852c87bfccf363e8f06f02fd5695
Naman Srivastava <namansrivastava1608@gmail.com>
2025-12-25 12:31:12 +0530

    delete

--- React/expressing.js
+++ React/expressing.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000
+app.use(express.json());
 app.get("/", async (req, res) => {
     try {
         const response = await fetch("https://dummyjson.com/c/3029-d29f-4014-9fb4")
@@ -10,6 +11,25 @@ app.get("/", async (req, res) => {
     } catch (error) {
         res.status(500).send("ISR")
     }
+})
+app.post("/post",async(req,res)=>{
+    const response=await fetch("https://dummyjson.com/posts/add ",
+        {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(req.body)
+        }
+    );
+    const data = await response.json();
+    res.json(data);
+})
+app.delete("/delete",async(req,res)=>{
+    const response=await fetch("https://dummyjson.com/posts/3",
+        {
+            method:"DELETE",
+        })
+    const data=await response.json();
+    res.json(data)
 })
 app.listen(port, () => {
     console.log(`Listening on port ${3000}`);
